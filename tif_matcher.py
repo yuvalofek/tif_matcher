c1b3b723c64c2e5fda70af16b659eb9a520687ec
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-10-25 23:17:00 -0400

    Update tif_matcher.py

--- tif_matcher.py
+++ tif_matcher.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dir1', type=str, help='Input dir 1')
 parser.add_argument('--dir2', type=str, help='Input dir 2')
 parser.add_argument('--out_file', type=str, default='./matched_tifs.json', help='Name of output file')
-
+args = parser.parse_args()
 
 
 def match_image(image_path, dir2):
@@ -33,12 +33,12 @@ def match_image(image_path, dir2):
 matches_th = []
 matches = []
 with cp.ThreadPoolExecutor() as ex:
-  for image in os.listdir(dir1):
-    image_path = os.path.join(dir1, image)
-    matches_th.append(ex.submit(match_image, image_path, dir2))
+  for image in os.listdir(args.dir1):
+    image_path = os.path.join(args.dir1, image)
+    matches_th.append(ex.submit(match_image, image_path, args.dir2))
 for match in cp.as_completed(matches_th):
     matches.append(match.result())
 
-
-with open(out_file, 'w') as wf:
+os.makedirs(args.out_file)
+with open(args.out_file, 'w') as wf:
   json.dump(matches, wf)
