e986da82da646a8abbdd5f2b6429aa2b528c2aab
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-10-25 22:06:26 -0400

    Add files via upload

---
+++ tif_matcher.py
@@ -0,0 +1,43 @@
+import os
+import rasterio
+import matplotlib.pyplot as plt
+import concurrent.futures as cp
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir1', type=str, help='')
+parser.add_argument('--dir1', type=str, help='')
+parser.add_argument('--delimiter', type=str, default='_', help='delimiter to split names ')
+parser.add_argument('--out_file', type=str, defualt='matched_tifs', help='Name of output file')
+
+
+
+def match_image(image_path, dir2, delimiter='_'):
+
+  with rasterio.open(image_path, 'r') as src:
+      img = src.read()
+  for image2 in os.listdir(dir2):
+    image2_path = os.path.join(dir1, image2)
+    with rasterio.open(image2_path, 'r') as src:
+        img2 = src.read()
+    if img2 == img:
+      print(image_path, image2_path)
+      # return matching sets
+      return [image_path.split(delimiter)[0], image2_path.split(delimiter)[0]]
+  return
+
+
+## Match the two datasets
+matches_th = []
+matches = []
+with cp.ThreadPoolExecutor() as ex:
+  for image in os.listdir(dir1):
+    image_path = os.path.join(dir1, image)
+    matches_th.append(ex.submit(match_image, image_path, dir2, delimiter='_'))
+for match in cp.as_completed(matches_th):
+    matches.append(match.result())
+
+
+with open(out_file, 'w') as wf:
+  json.dump(matches, wf)
