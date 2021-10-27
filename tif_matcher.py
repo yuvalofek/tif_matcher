e2002f38478bc042410c3321b500206d98cec4f4
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-10-27 19:13:36 -0400

    add normalization and verbose

--- tif_matcher.py
+++ tif_matcher.py
@@ -10,23 +10,26 @@ parser.add_argument('--dir2', type=str, help='Input dir 2')
 parser.add_argument('--out_file', type=str, default='./matched_tifs.json', help='Name of output file')
 parser.add_argument('--key_word1', type=str, default='', help='Key word to select only some images from dir1')
 parser.add_argument('--key_word2', type=str, default='', help='Key word to select only some images from dir2')
+parser.add_argument('--normalize', action='store_true', help='Match normalized images')
+parser.add_argument('--verbose', action='store_true', help='Print matches')
 
 args = parser.parse_args()
 
 
-def match_image(image_path, dir2, key_word):
+def match_image(img, image_path, dir2, key_word, normalize=False, verbose=False):
   """
   Find image path in dir2 that matches image in image_path
   """
-  with rasterio.open(image_path, 'r') as src:
-      img = src.read()
   for image2 in os.listdir(dir2):
     if key_word not in image2:
       continue
     image2_path = os.path.join(dir2, image2)
     with rasterio.open(image2_path, 'r') as src:
         img2 = src.read()
+    if normalize:
+      img2=img2/img2.max()
     if (img2 == img).all():
+      if args.verbose
       print(image_path, image2_path)
       # return matching sets
       return [image_path, image2_path]
@@ -41,7 +44,11 @@ with cp.ThreadPoolExecutor() as ex:
     if args.key_word1 not in image:
       continue
     image_path = os.path.join(args.dir1, image)
-    matches_th.append(ex.submit(match_image, image_path, args.dir2, args.key_word2))
+    with rasterio.open(image_path, 'r') as src:
+      img = src.read()
+    if args.normalize:
+      img=img/img.max()
+    matches_th.append(ex.submit(match_image, img, image_path, args.dir2, args.key_word2, args.normalize, args.verbose))
 for match in cp.as_completed(matches_th):
     matches.append(match.result())
 
